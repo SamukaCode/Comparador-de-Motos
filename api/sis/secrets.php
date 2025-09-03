84871558d55d51f990ff11635da1a8b8e39b9c47
Samuel Camargo <samuelpcamargo2005@gmail.com>
2025-09-03 14:57:29 -0300

    Update secrets.php

--- api/sis/secrets.php
+++ api/sis/secrets.php
@@ -3,5 +3,6 @@ return [
   'DB_HOST'     => 'ep-wispy-darkness-a55dfxbd-pooler.us-east-2.aws.neon.tech',
   'DB_NAME'     => 'neondb',
   'DB_USER'     => 'neondb_owner',
-  'DB_PASS'     => 'npg_PxC3ots8mLBj'
+  'DB_PASS'     => 'npg_PxC3ots8mLBj',
+  'DB_ENDPOINT' => 'ep-wispy-darkness-a55dfxbd'
 ];
