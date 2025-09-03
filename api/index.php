870310d9f5debe9cf56e206c2cbcdfc28b2ba6dd
Samuel Camargo <samuelpcamargo2005@gmail.com>
2025-09-03 15:10:02 -0300

    .

--- api/index.php
+++ api/index.php
@@ -13,7 +13,7 @@ $marcas = listarMarcas($pdo);
   <meta charset="utf-8">
   <title>Duas Rodas - Comparador de Motos</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
+<link rel="icon" href="/assets/icons/moto-fav.ico">  
   <!-- Bootstrap 5 CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
